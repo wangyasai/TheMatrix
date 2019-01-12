e1cf9e070f6164c8c92e42d4b36f1cc19d47e37c
yasai wang <yasaiwang@yasaideMacBook-Pro.local>
2019-01-13 00:14:03 +0800

    update

--- js/sketch.js
+++ js/sketch.js
@@ -1,9 +1,9 @@
 var streams = [];
 var fadeInterval = 1.5;
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(options.Background);
+var myCanvas;
 
+function setup() {
+  myCanvas = createCanvas(windowWidth, windowHeight);
 
   var x = 0;
   for (var i = 0; i <= width / options.Size; i++) {
@@ -12,18 +12,27 @@ function setup() {
     streams.push(stream);
     x += options.Size;
   }
-
-  textFont('Consolas');
-
 }
 
+
+
 function draw() {
-   background(options.Background[0],options.Background[1],options.Background[2],options.Opacity);
+    drawMatrix();
+}
+
+
+function drawMatrix(){
+ background(options.Background[0],options.Background[1],options.Background[2],options.Opacity);
+  if(options.isPNG == true){
+    clear();
+}
+
    textSize(options.Size);
    streams.forEach(function(stream) {
    stream.render();
   });
 }
+
 
 function Symbol(x, y, speed, first, opacity) {
   this.x = x;
@@ -34,25 +43,45 @@ function Symbol(x, y, speed, first, opacity) {
   this.first = first;
   this.opacity = opacity;
 
-  this.switchInterval = round(random(2, 25));
-
   this.setToRandomSymbol = function() {
     var charType = round(random(0, 5));
-    if (frameCount % this.switchInterval == 0) {
-      if (charType > 1) {
-        // set it to Katakana
-        this.value = String.fromCharCode(
-          0x30A0 + round(random(0, 96))
-        );
-      } else {
-        // set it to numeric
-        this.value = round(random(0,9));
+    var switchInterval = round(random(50-options.TextSpeed, 50));
+    if (frameCount % switchInterval == 0) {
+       if(options.Unicode == 'Katakana'){
+           this.value = String.fromCharCode(            
+              0x30A1 + round(random(0, 96))       
+           );
+      
+         } else if(options.Unicode == 'BoxDrawing'){
+          this.value = String.fromCharCode(
+              0x2500 + round(random(0, 96))
+           );
+        } else if(options.Unicode == 'Braille'){
+          this.value = String.fromCharCode(
+              0x2803+ round(random(0, 60))
+           );
+        } else if(options.Unicode == 'CJK Radicals Supplement'){
+          this.value = String.fromCharCode(
+              0x2E81+ round(random(0, 60))
+           );
+        }else if(options.Unicode == 'Kanbun'){
+          this.value = String.fromCharCode(
+             0x3190+ round(random(0, 11))
+           );
+        }else if(options.Unicode == 'Yi Syllables'){
+          this.value = String.fromCharCode(
+             0xA000+ round(random(0, 200))
+           );
+        } else if(options.Unicode  == "Text"){
+          var s = options.Text;
+          var l = s.length;
+          this.value = s.charAt(random(0,l));       
+        }
       }
     }
-  }
 
   this.rain = function() {
-    this.y = (this.y >= height) ? 0 : this.y += options.Speed;
+    this.y = (this.y >= height) ? 0 : this.y += options.DropSpeed;
   }
 
 }
@@ -60,7 +89,8 @@ function Symbol(x, y, speed, first, opacity) {
 function Stream() {
   this.symbols = [];
   this.totalSymbols = round(random(5, 35));
-  this.speed = random(options.Speed);
+  this.speed = random(options.DropSpeed);
+
 
   this.generateSymbols = function(x, y) {
     var opacity = 255;
@@ -81,12 +111,13 @@ function Stream() {
     }
   }
 
+
   this.render = function() {
     this.symbols.forEach(function(symbol) {
       if (symbol.first) {
-        fill(options.Color1[0], options.Color1[1],options.Color1[2],symbol.opacity);
+        fill(options.Color[0], options.Color[1],options.Color[2],symbol.opacity);
       } else {
-        fill(options.Color2[0], options.Color2[1],options.Color2[2], symbol.opacity);
+        fill(options.Color[0]+50, options.Color[1]+50,options.Color[2]+50, symbol.opacity);
       }
       text(symbol.value, symbol.x, symbol.y);
       symbol.rain();
